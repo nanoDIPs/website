dc3b2313dd4726c3bba5730ca8747458f7b0b773
Georg K A Wachter <gkw16@ic.ac.uk>
2019-09-04 13:00:30 +0100

    navbar changes

---
+++ assets/navbarScript.js
@@ -0,0 +1,8 @@
+function navbarfunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
